f4608cd27cfd616591f3ddaaaf159efccf0b6877
aulin1 <audrey.youran.lin@gmail.com>
2024-03-12 21:55:33 -0400

    Beginning coroutine functionality

--- app/src/main/java/com/cs4520/assignment4/ProductViewModel.kt
+++ app/src/main/java/com/cs4520/assignment4/ProductViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Response
@@ -30,22 +31,23 @@ class ProductViewModel() : ViewModel() {
 
 
     private fun makeApiCall(input: String?=null) {
-        val data = RetrofitClient.getRetrofitInstance().create(ApiEndPoint::class.java)
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.getAllRepository()
+                .enqueue(object : retrofit2.Callback<ArrayList<ProductData>> {
+                    override fun onFailure(call: Call<ArrayList<ProductData>>, t: Throwable) {
+                        Log.d("Testing API", t.toString())
+                        _ResponseData.value = null
+                    }
 
-        repository.getAllRepository().enqueue(object : retrofit2.Callback<ArrayList<ProductData>> {
-            override fun onFailure(call: Call<ArrayList<ProductData>>, t: Throwable) {
-                Log.d("Testing API", t.toString())
-                _ResponseData.value = null
-            }
-
-            override fun onResponse(
-                call: Call<ArrayList<ProductData>>,
-                response: Response<ArrayList<ProductData>>
-            ) {
-                Log.d("Testing API", "Succeeded")
-                if (!response.isSuccessful) _ResponseData.value =
-                    null else _ResponseData.value = response.body()
-            }
-        })
+                    override fun onResponse(
+                        call: Call<ArrayList<ProductData>>,
+                        response: Response<ArrayList<ProductData>>
+                    ) {
+                        Log.d("Testing API", "Succeeded")
+                        if (!response.isSuccessful) _ResponseData.value =
+                            null else _ResponseData.value = response.body()
+                    }
+                })
+        }
     }
 }
